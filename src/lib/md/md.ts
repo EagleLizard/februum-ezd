d2b78d518da5effc00965c581bb2cd8e21f59338
eaglelizard <whitedtd@gmail.com>
2025-02-17 07:49:50 -0700

    some ast traversal

--- src/lib/md/md.ts
+++ src/lib/md/md.ts
@@ -18,15 +18,58 @@ export async function md() {
   let fileData = fs.readFileSync(test1FilePath).toString();
   let mdr = new commonmark.Parser();
   let parsed = mdr.parse(fileData);
-  traverse(parsed);
+  traverse2(parsed);
+}
+
+function traverse2(cmNode: commonmark.Node) {
+  function helper(currNode: commonmark.Node | null, soFar?: commonmark.Node[]) {
+    soFar = soFar ?? [];
+    if(currNode === null) {
+      return;
+    }
+    let depth = soFar.length - 1;
+    // console.log(`\n${currNode.type} - ${depth}`);
+    if(currNode.type === 'text') {
+      // console.log(currNode.literal);
+    } else {
+      let soFarStr = soFar.map(sfNode => sfNode.type).join(', ')
+      console.log(`${soFarStr} - ${currNode.type}`)
+    }
+    switch(currNode.type) {
+      case 'text':
+        console.log(currNode.literal);
+        break;
+      case 'paragraph':
+        console.log('<p>');
+        soFar.pop();
+        break;
+    }
+    soFar = [ ...soFar, currNode ];
+    helper(currNode.firstChild, soFar);
+    helper(currNode.next, soFar);
+    soFar.pop();
+  } 
+  helper(cmNode);
 }
 
 function traverse(cmNode: commonmark.Node) {
   console.log(cmNode);
   console.log(cmNode.type);
-  let walker = cmNode.walker();
-  let walkRes: commonmark.NodeWalkingStep | null;
-  while((walkRes = walker.next()) !== null) {
-    console.log(walkRes.node.type);
+  let depth = 0;
+  // let currNode: commonmark.Node | null;
+  // currNode = cmNode;
+  let iters = 0;
+  let queue: commonmark.Node[] = [ cmNode ];
+  while(queue.length > 0) {
+    let currNode: commonmark.Node = queue.shift()!;
+    if(currNode.firstChild !== null) {
+      queue.push(currNode);
+    }
+    console.log(`i: ${iters}, depth: ${depth}`);
+    if(iters++ > 40) {
+      break;
+    }
   }
 }
+
+
